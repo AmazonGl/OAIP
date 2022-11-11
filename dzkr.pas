28485e85e1a8defc12f02f0d1be9d88610cce235
Amazon <87614827+AmazonGl@users.noreply.github.com>
2022-11-11 12:08:58 +0300

    Add files via upload

---
+++ dzkr.pas
@@ -0,0 +1,23 @@
+﻿const chis = 6;
+var x: array of integer;
+begin
+ Setlength(x,chis);
+ var max:=1;
+ var min:=1;
+  for var i := 1 to 5 do Read(x[i]);
+  begin
+    max:=x[1];
+    min:=x[1];
+  end;
+ for var i:=1 to 5 do begin
+   if x[i]>max then
+      max:=x[i];
+   end;
+ for var i:=1 to 5 do begin  
+   if (x[i]<min) then
+      min:=x[i];
+   end;
+Writeln('Максимальный элемент = ', max);
+Writeln('Минимальный элемент = ', min);
+Write(x);
+  end.
---
+++ dzkr2.pas
@@ -0,0 +1,8 @@
+﻿begin
+  var c:=0;
+  var a:=ReadString('Введите строку = ');
+  var b:=ReadString('Введите подстроку = ');
+  for var i := 1 to length(a) do
+    if (pos(a[i],b) > 0) then inc(c);
+  Writeln(c);
+end.
