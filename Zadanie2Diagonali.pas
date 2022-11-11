04e9e272ed1ff0a80e4d9ff496d679880660098b
Amazon <87614827+AmazonGl@users.noreply.github.com>
2022-11-11 20:52:28 +0300

    Add files via upload

---
+++ Zadanie2Diagonali.pas
@@ -0,0 +1,28 @@
+﻿var i,j,k,s,n: integer;
+x: array[1..4,1..4] of integer;
+begin
+s:=1;
+k:=1;
+Readln(n);
+for i:=1 to n do begin
+  for j:=1 to n do begin
+    x[i, j]:=random(10);
+    write(x[i, j]:3);
+end;
+writeln;
+end;
+j:=n+1;
+for i:=1 to n do begin
+j:=j-1;   
+s:=s*x[i,j];
+end;
+j:=n+1;
+for i:=n downto 1 do begin
+j:=j-1;   
+k:=k*x[i,j];
+end;
+n:=s*k;
+writeln('Произведение чисел на диагоналях = ', n);
+writeln('Произведение побочной диагонали = ', s);
+writeln('Произведение главной диагонали = ', k);
+end.
---
+++ Zadanie3Edinich.pas
@@ -0,0 +1,26 @@
+﻿var i,j,k,s,n: integer;
+x: array[1..4,1..4] of integer;
+begin
+k:=0;
+s:=0;
+writeln('Введите элементы матрицы');
+Readln(n);
+for i:=1 to n do begin
+  for j:=1 to n do begin
+    x[i, j]:=random(10);
+    write(x[i,j]:3);
+    end;
+    writeln;
+end;
+for i:=1 to n do
+for j:=1 to n do
+ begin
+  if x[i,j]=0 then inc(k);
+  if i=j then
+   begin
+    if x[i,j]=1 then inc(s)
+   end;
+ end;
+if (s=n)and(k=n*n-n)then writeln('Матрица единичная')
+else writeln('Матрица не единичная')
+end.
