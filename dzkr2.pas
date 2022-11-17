305055edef9224e9278db94d0260162e594bae48
Amazon <87614827+AmazonGl@users.noreply.github.com>
2022-11-17 12:16:43 +0300

    Add files via upload

---
+++ dzkr2.pas
@@ -0,0 +1,13 @@
+﻿begin
+  var x:=0;
+  var c:=0;
+  var a:=ReadString('Введите строку = ');
+  var b:=ReadString('Введите подстроку = ');
+  for var i := 1 to length(a) do begin
+    for var j:= 1 to length(b) do
+    if a[i+j-1]=b[j] then x:=1 else begin x:=0; break; end;
+    if x=1 then inc(c);
+    x:=0;
+    end;
+  Println(c);
+end.
