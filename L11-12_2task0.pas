687a9e57c39d66d9fe20a1be0348dd530c210085
Amazon <87614827+AmazonGl@users.noreply.github.com>
2022-12-01 11:34:43 +0300

    Add files via upload

---
+++ L11-12_2task0.pas
@@ -0,0 +1,14 @@
+﻿uses GraphABC;
+begin
+SetPenColor(clgreen);
+MoveTo (500, 200);
+LineTo (600, 200);
+LineTo (600, 300);
+LineTo (500, 300);
+LineTo (500, 200);
+SetPenColor(clred);
+MoveTo (100, 200);
+LineTo (200, 200);
+LineTo (150, 100);
+LineTo (100, 200);
+end.
---
+++ L11-12_2task2.pas
@@ -0,0 +1,24 @@
+﻿uses GraphABC;
+begin
+  moveTo(250,400);
+  lineTo(350,400);
+  lineto(300,200);
+  lineto(250,400);
+  floodfill(300,300,clred);
+  Circle(300,200,20);
+  FloodFill(300,200,clred);
+  moveTo(300,400);
+  lineTo(400,400);
+  lineto(450,200);
+  lineto(335,347);
+  floodfill(400,390,clgreen);
+  Circle(452,200,20);
+  FloodFill(452,200,clgreen);
+  moveTo(300,400);
+  lineTo(200,400);
+  lineto(150,200);
+  lineto(265,347);
+  floodfill(200,390,clblue);
+  Circle(150,200,20);
+  FloodFill(150,200,clblue);
+end.
