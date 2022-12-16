1f3bc6acf2f3ac5cfbdbfae1ea565013b2a71823
Amazon <87614827+AmazonGl@users.noreply.github.com>
2022-12-16 13:30:08 +0300

    Add files via upload

---
+++ 13_Zadanie1.pas
@@ -0,0 +1,14 @@
+﻿var myfile:text; a:string;
+begin
+assign(myfile,'G:\Колледж\2 курс\ОАиП\PascalABC\!Лабораторные работы\Лабораторная №13-14\Файлы\13_Zadanie1.txt');
+rewrite(myfile);
+for var i:=1 to 10 do
+    print(myfile,i,' ');
+    close(myfile);
+    reset(myfile);
+for var i:=1 to 10 do begin
+    read(myfile,a);
+    println(a);
+end;
+close(myfile);
+end.
